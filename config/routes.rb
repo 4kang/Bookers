1c73777c820418be46d64a53228e220c8aea588c
4kang <106650699+4kang@users.noreply.github.com>
2022-06-11 06:51:57 +0000

    [Add]4-12

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'books/new'
+  get '/' => 'homes#top'
   post 'books' => 'books#create'
-  get 'books/index' => 'books#index'
+  get 'books' => 'books#index'
   get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  get 'top' => 'homes#top'
   patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   resources :books
