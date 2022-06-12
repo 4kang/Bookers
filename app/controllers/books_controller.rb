ff43e1ad2f1ff0f833963053ac762f939fa1bfd1
4kang <106650699+4kang@users.noreply.github.com>
2022-06-12 06:37:18 +0000

    [Add]4-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,12 +6,10 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    if @book.save
-      redirect_to '/books'
-    else
-      render :new
-    end
+    @book.save
+    redirect_to book_path(@book.id)
   end
+    
 
   def show
     @book = Book.find(params[:id])
